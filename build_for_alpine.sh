d79016b4a7fecbcc94e5eb8b6765a5b7fb95b35d
Steven Elliott <seeyallc6c@gmail.com>
2026-06-22 03:53:24 +0000

    testing

--- build_for_alpine.sh
+++ build_for_alpine.sh
@@ -1,5 +1,7 @@
 podman run --rm -it \
   -v /home/seev0/num/1-c0d3/rust:/home/seev0/etc:Z \
-  -e CARGO_TARGET_DIR=/home/seev0/etc/tmp \
+  -e CARGO_TARGET_DIR=/home/seev0/etc/trade/bin/alpine_usta/ \
   alpine-rust-builder:latest \
   cargo build --release --manifest-path /home/seev0/etc/trade/Cargo.toml -p usta
+
+scp {bin/alpine_usta/release/*.pem,bin/alpine_usta/release/usta} root@192.168.122.239:/var/trade/usta
